6fe631edb7f5fd0789102bea7fa7d15728e139b0
oleg578 <oleg.nagornij@gmail.com>
2025-11-02 21:59:46 +0100

    some fix with icons

--- assets/js/app-remote.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const target = document.getElementById('app-remote');
-  if (target) {
-    target.innerHTML = 'Message from remote script';
-  }
-});
